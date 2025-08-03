cacef3a83415b64c023a4f87c3f2e01c08a2c077
Astrid-Beyer <astrid.beyer@orange.fr>
2025-08-03 20:12:34 +0200

    FEAT: consolidate IFF processing to single output file (process multiple IFF files into one all_files.strings.txt + headers for source id)

--- src/main.c
+++ src/main.c
@@ -114,7 +114,7 @@ static char *get_out_filename(const char *in_filename)
 }
 
 
-void export_text_chunk(FILE *fp, u8 *text_chunk, u32 offset)
+void export_text_chunk(FILE *fp, u8 *text_chunk, u32 offset, const char *filename)
 {
     TEXT_CHUNK_HEADER *tch;
     TEXT_CHUNK_ENTRY *tce;
@@ -125,10 +125,12 @@ void export_text_chunk(FILE *fp, u8 *text_chunk, u32 offset)
 
     fprintf(
         fp,
+        "\n=== FILE: %s ===\n" \
         "--- TEXT CHUNK ---\n" \
         "Language: %s\n" \
         "Strings count: %u\n" \
         "File offset: %08X\n\n",
+        filename,
         (char *)(text_chunk + tch->language_offset),
         tch->num_strings,
         offset
@@ -173,93 +175,115 @@ int main(int argc, char **argv)
     u32 actual_unpack_size;
     u32 num_text_chunks;
     u32 num_chunks;
+    u32 total_text_chunks = 0;
+    u32 total_chunks = 0;
     IFF_CHUNK_HEADER *ch;
 
-    file_map = map_file(argv[1], &file_size);
-    if (!file_map)
-    {
-        fprintf(stderr, "\nERROR: Input file cannot be read properly\n");
-        goto F_OUT;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s file1.iff [file2.iff ...]\n", argv[0]);
+        return 1;
     }
-    
-    out_filename = get_out_filename(argv[1]);
+
+    // create fixed output filename
+    size_t out_filename_len = strlen("all_files") + strlen(STRINGS_FILE_SUFFIX) + 1;
+    out_filename = (char *)malloc(out_filename_len);
     if (!out_filename)
     {
         fprintf(stderr, "\nERROR: Output filename cannot be created properly\n");
-        goto F_FREE_MAP;
+        return 1;
     }
-
-    debug_init(argv[1], out_filename, file_size, file_map);
+    snprintf(out_filename, out_filename_len, "%s%s", "all_files", STRINGS_FILE_SUFFIX);
 
     fp = fopen(out_filename, "w+b");
     if (!fp)
     {
         fprintf(stderr, "\nERROR: Could not create output file\n");
-        goto F_FREE_NAME;
+        free(out_filename);
+        return 1;
     }
 
-    offset = 0;
-    num_chunks = 0;
-    num_text_chunks = 0;
-
-    while (offset < file_size)
-    {
-        ch = (IFF_CHUNK_HEADER *)(file_map + offset);
-        num_packed_segments = (ch->unpack_size > 0) * ((ch->unpack_size >> 0xF) + ((ch->unpack_size & 0x7FFF) != 0));
-
-        ++num_chunks;
-
-        // Display debug information for all chunk types
-        // debug_chunk(ch, num_packed_segments, offset);
-            
-        if (ch->id != CKID_TEXT) goto NEXT_CHUNK;
-
-        ++num_text_chunks;
-
-        // Display debug information for TEXT chunks only
-        debug_chunk(ch, num_packed_segments, offset);
-
-        if (num_packed_segments > 0)
+    // process each input file
+    for (int file_idx = 1; file_idx < argc; file_idx++) {
+        fprintf(stderr, "\n=== Processing file %d/%d: %s ===\n", file_idx, argc-1, argv[file_idx]);
+        
+        file_map = map_file(argv[file_idx], &file_size);
+        file_map = map_file(argv[file_idx], &file_size);
+        if (!file_map)
         {
-            unpack_buf = (u8 *)malloc(ch->unpack_size);
-            if (!unpack_buf)
-            {
-                fprintf(stderr, "ERROR: Memory allocation failed for current TEXT chunk\n");
-                goto NEXT_CHUNK;
-            }
-
-            segment_offset = offset + sizeof(IFF_CHUNK_HEADER) + (num_packed_segments * sizeof(u32));
-            segments_sizes = (u32 *)(file_map + offset + sizeof(IFF_CHUNK_HEADER));
-            unpack_buf_offset = 0;
-
-            for (s32 i = 0; i < num_packed_segments; ++i)
-            {
-                actual_unpack_size = unlzvc_ghidra(file_map + segment_offset, unpack_buf + unpack_buf_offset);
-                segment_offset += segments_sizes[i];
-                unpack_buf_offset += actual_unpack_size;
-                debug_unpack(segment_offset, unpack_buf_offset, actual_unpack_size, i, segments_sizes);
-            }
-            
-            text_chunk = unpack_buf;
-        }
-        else
-        {
-            text_chunk = file_map + offset + sizeof(IFF_CHUNK_HEADER);
+            fprintf(stderr, "\nERROR: Input file %s cannot be read properly, skipping...\n", argv[file_idx]);
+            continue;
         }
 
-        export_text_chunk(fp, text_chunk, offset);
+        debug_init(argv[file_idx], out_filename, file_size, file_map);
 
-NEXT_CHUNK:
-        offset += (sizeof(IFF_CHUNK_HEADER) + ch->size);
+        offset = 0;
+        num_chunks = 0;
+        num_text_chunks = 0;
+
+        while (offset < file_size)
+        {
+            ch = (IFF_CHUNK_HEADER *)(file_map + offset);
+            num_packed_segments = (ch->unpack_size > 0) * ((ch->unpack_size >> 0xF) + ((ch->unpack_size & 0x7FFF) != 0));
+
+            ++num_chunks;
+
+            // Display debug information for all chunk types
+            // debug_chunk(ch, num_packed_segments, offset);
+                
+            if (ch->id != CKID_TEXT) goto NEXT_CHUNK;
+
+            ++num_text_chunks;
+
+            // Display debug information for TEXT chunks only
+            debug_chunk(ch, num_packed_segments, offset);
+
+            if (num_packed_segments > 0)
+            {
+                unpack_buf = (u8 *)malloc(ch->unpack_size);
+                if (!unpack_buf)
+                {
+                    fprintf(stderr, "ERROR: Memory allocation failed for current TEXT chunk\n");
+                    goto NEXT_CHUNK;
+                }
+
+                segment_offset = offset + sizeof(IFF_CHUNK_HEADER) + (num_packed_segments * sizeof(u32));
+                segments_sizes = (u32 *)(file_map + offset + sizeof(IFF_CHUNK_HEADER));
+                unpack_buf_offset = 0;
+
+                for (s32 i = 0; i < num_packed_segments; ++i)
+                {
+                    actual_unpack_size = unlzvc_ghidra(file_map + segment_offset, unpack_buf + unpack_buf_offset);
+                    segment_offset += segments_sizes[i];
+                    unpack_buf_offset += actual_unpack_size;
+                    debug_unpack(segment_offset, unpack_buf_offset, actual_unpack_size, i, segments_sizes);
+                }
+                
+                text_chunk = unpack_buf;
+            }
+            else
+            {
+                text_chunk = file_map + offset + sizeof(IFF_CHUNK_HEADER);
+            }
+
+            export_text_chunk(fp, text_chunk, offset, argv[file_idx]);
+
+    NEXT_CHUNK:
+            offset += (sizeof(IFF_CHUNK_HEADER) + ch->size);
+        }
+
+        fprintf(stderr, "File %s: Found %u TEXT chunk(s) (Total chunks: %u)\n", argv[file_idx], num_text_chunks, num_chunks);
+        total_text_chunks += num_text_chunks;
+        total_chunks += num_chunks;
+        
+        free(file_map);
     }
 
     fclose(fp);
-    fprintf(stderr, "Found %u TEXT chunk(s) (Total chunks: %u)\n", num_text_chunks, num_chunks);
+    fprintf(stderr, "\n=== SUMMARY ===\n");
+    fprintf(stderr, "Processed %d files\n", argc-1);
+    fprintf(stderr, "Total TEXT chunks: %u (Total chunks: %u)\n", total_text_chunks, total_chunks);
+    fprintf(stderr, "Output written to: %s\n", out_filename);
 
-F_FREE_NAME:
     free(out_filename);
-F_FREE_MAP:
-    free(file_map);
-F_OUT:
     return 0;
 }
